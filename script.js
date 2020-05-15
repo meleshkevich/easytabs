185876be8d1f413af5096f010e1684ad41b96ccb
Ilja home <i.meleshkevich@gmail.com>
2020-05-15 22:54:23 +0200

    adding easy tabs functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    // get DOM elements from html
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    // hide all unused content
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+    // show required content iso hidden
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+    // show required content after certain event for certain place - click
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
+
